37379c5de4ad99010898196523febaa612f507db
Andrew <andrew.cloke@googlemail.com>
2021-12-23 19:26:20 +0000

    Missing sequence number

--- missing-seq-item.py
+++ missing-seq-item.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+import random
 
-def CreateTestSeq(ListSize):
-    TestSeq=[i for i in range(ListSize)]
+def CreateTestSeq(listSize):
+    testSeq=[i for i in range(listSize)]
+    del testSeq[random.randint(0,listSize-1)]
+    return testSeq
+
+seqLength=100
+a=CreateTestSeq(seqLength)
+print(a)
+
+found=False
+i=seqLength-1
+offset=0
+while not found:
+    if i==int(i/2):
+        found=True
+    i=int(i/2)
+    if a[offset+i]>offset+i:  # if True, gap must be in first half
+        print("Missing number in first half ",i,offset)
+    else:
+        offset+=i
+        print("Missing number in second half ",i,offset)
+
+print("Missing number is ",offset+1)
+
