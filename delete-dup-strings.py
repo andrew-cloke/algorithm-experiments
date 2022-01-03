663dc23b4e408a02812dd46da9e5904546d2d0c7
Andrew <andrew.cloke@googlemail.com>
2022-01-03 21:33:56 +0000

    Adding delete-dup-strings.py

---
+++ delete-dup-strings.py
@@ -0,0 +1,20 @@
+# Delete duplicate strings in a list
+
+def removeDupStrings(s):
+    words=[]
+    word=""
+    s+=" "
+    for c in s:
+        if c==" ":
+            if len(words)>0 and words[-1]==word:
+                words.pop()
+            else:
+                words.append(word)
+            word=""
+        else:
+            word=word+c
+    return len(words),words
+
+print(removeDupStrings("one two two three"))
+print(removeDupStrings("tom jerry jerry tom"))
+print(removeDupStrings("ab aa aa bcd ab"))
