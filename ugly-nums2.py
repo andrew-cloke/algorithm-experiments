8c5608271179a52b1e5289ee8bc37a6cf325a88a
Andrew Cloke <andrew.cloke@googlemail.com>
2022-01-11 20:42:48 +0000

    Second ugly number approach

---
+++ ugly-nums2.py
@@ -0,0 +1,27 @@
+# Find the first 'n' ugly numbers - take two
+
+def find_ugly_nums(n):
+    l=[]
+    i2=0
+    i3=0
+    i5=0
+    l.append(1)
+    print(l[-1]," ",end="")
+    while len(l)<n:
+        next=min(l[i2]*2, l[i3]*3, l[i5]*5)
+        l.append(next)
+        print(l[-1]," ",end="")
+
+        if next==l[i2]*2:
+            i2+=1
+        if next==l[i3]*3:
+            i3+=1
+        if next==l[i5]*5:
+            i5+=1
+    return l
+
+result=find_ugly_nums(150)
+print("\n",result)
+
+        
+
