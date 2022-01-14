c33036446181bcb859b99c7de414953595a49dd5
Andrew Cloke <andrew.cloke@googlemail.com>
2022-01-14 21:29:52 +0000

    Fixing up recrusive travelling salesman

--- travelling-salesman-recursive.py
+++ travelling-salesman-recursive.py
@@ -9,17 +9,20 @@ def allVisited(a):
 def tsp(graph,curr_pos,visited,cost):
     global nodes
     global ans
+    global path
 
     if allVisited(visited):
         cost+=graph[curr_pos][0]
         ans.append(cost)
+        print("Final cost",cost,"Path",path)
         return
     
     for i in range(nodes):
         if not visited[i]:
-            cost+=graph[curr_pos][i]
+            path.append((curr_pos+1,i+1))
             visited[i]=True
-            tsp(graph,i,visited,cost)
+            tsp(graph,i,visited,cost+graph[curr_pos][i])
+            path.pop()
             visited[i]=False
 
 
@@ -27,9 +30,10 @@ def tsp(graph,curr_pos,visited,cost):
 if __name__ == "__main__":
     nodes=4
     ans=[]
+    path=[]
     # matrix representation of graph
     graph = [[0, 10, 15, 20], [10, 0, 35, 25],
             [15, 35, 0, 30], [20, 25, 30, 0]]
-    visited=[ False,False,False,False ]
+    visited=[ True,False,False,False ]
     tsp(graph, 0, visited,0)
     print(ans)
