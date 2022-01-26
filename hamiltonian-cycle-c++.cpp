521b3277108d1ce6b1ebb1f0219feb81a797978e
Andrew Cloke <andrew.cloke@googlemail.com>
2022-01-26 20:29:15 +0000

    First working c++ hamiltonian cycle

--- hamiltonian-cycle-c++.cpp
+++ hamiltonian-cycle-c++.cpp
@@ -1,14 +1,15 @@
 // Hamiltonian cycle in c++
+// https://www.geeksforgeeks.org/hamiltonian-cycle-backtracking-6/?ref=lbp
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void tryNextStep(int curpos,vector<int>& path, bool* visited,const int** graph) {
+void tryNextStep(int curpos,vector<int>& path, bool* visited,const int graph[5][5]) {
     bool allVisited=true;
     int i;
-    for(int i=0;i<5;i++) {
+    for(i=0;i<5;i++) {
         if(!visited[i]) {
             allVisited=false;
         }
@@ -20,13 +21,23 @@ void tryNextStep(int curpos,vector<int>& path, bool* visited,const int** graph) 
             for(i=0;i<size;i++) {
                 cout << path[i] << " ";
             }
+            cout << endl;
             path.pop_back();
+        }
+    }
+    for(i=0;i<5;i++) {
+        if(!visited[i] && graph[curpos][i]==1) {
+            visited[i]=true;
+            path.push_back(i);
+            tryNextStep(i,path,visited,graph);
+            path.pop_back();
+            visited[i]=false;
         }
     }
 }
 
 int main() {
-    const int graph[][]={
+    const int graph[5][5]={
         {0,1,0,1,0},
         {1,0,1,1,1},
         {0,1,0,0,1},
@@ -35,8 +46,6 @@ int main() {
     const int nodes=5;
     bool visited[]={true,false,false,false,false};
     vector<int> path;
-    
-
+    path.push_back(0);
     tryNextStep(0,path,visited,graph);
-
 }
