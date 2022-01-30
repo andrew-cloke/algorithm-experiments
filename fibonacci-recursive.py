5c06230286b21834daa0212ec363f7fd770da9fe
Andrew Cloke <andrew.cloke@googlemail.com>
2022-01-30 21:28:45 +0000

    Fixed up recursive fibonacci

--- fibonacci-recursive.py
+++ fibonacci-recursive.py
@@ -3,9 +3,9 @@
 def fibonacci(n2,n1,count):
     if count!=0:
         sum=n2+n1
-        print(sum," ")
+        print(sum," ", end="")
         fibonacci(n1,sum,count-1)
 
-print("1 1 ")
+print("1 1 ", end="")
 fibonacci(1,1,10)
 print("\n")
