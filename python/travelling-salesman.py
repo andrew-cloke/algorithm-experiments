a4e919db941531db689148337c911ab6ed33dd48
Andrew Cloke <andrew.cloke@googlemail.com>
2022-02-19 22:40:46 +0000

    Working on an implementation of Dijkstra algorithm

--- python/travelling-salesman.py
+++ python/travelling-salesman.py
@@ -38,7 +38,9 @@ def travellingSalesmanProblem(graph, s):
 if __name__ == "__main__":
 
 	# matrix representation of graph
-	graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-			[15, 35, 0, 30], [20, 25, 30, 0]]
+	graph = [[0, 10, 15, 20],
+			 [10, 0, 35, 25],
+			 [15, 35, 0, 30],
+			 [20, 25, 30, 0]]
 	s = 0
 	print(travellingSalesmanProblem(graph, s))
