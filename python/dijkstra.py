9856b208d0658ba7da498302ff277be1b9f5f6c8
Andrew Cloke <andrew.cloke@googlemail.com>
2022-02-20 21:24:35 +0000

    Another update to Dijkstra algorithm.

--- python/dijkstra.py
+++ python/dijkstra.py
@@ -1,13 +1,7 @@
 # Minimum path by Dijkstra's algorithm
+# https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 
 V=9
-
-def shortest_path(graph,start,end):
-        visited=[-1]*V
-        path=[1000]*V
-        
-
-
 graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
         [4, 0, 8, 0, 0, 0, 0, 11, 0],
         [0, 8, 0, 7, 0, 4, 0, 0, 2],
@@ -18,3 +12,19 @@ graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
         [8, 11, 0, 0, 0, 0, 1, 0, 7],
         [0, 0, 2, 0, 0, 0, 6, 7, 0]
         ]
+visited=[-1]*V
+distance=[1000]*V
+u=0
+distance[u]=0
+visited[u]=0
+q=[i for i in range(0,9)]
+while len(q)>0:
+        u=q.pop(0)
+        print(q,visited,distance)
+        for v in q:
+                a=distance[u]+graph[u][v]
+                if graph[u][v]>0 and a<distance[v]:
+                        distance[v]=a
+                        visited[v]=0
+
+print(q,visited,distance)
