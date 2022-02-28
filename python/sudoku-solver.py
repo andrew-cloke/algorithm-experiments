1a09b273cb54a1d423087a27b2bce44db6366eb5
Andrew Cloke <andrew.cloke@googlemail.com>
2022-02-28 21:33:03 +0000

    First attempt

--- python/sudoku-solver.py
+++ python/sudoku-solver.py
@@ -27,6 +27,40 @@ def createPossibilityMatrix(s):
         matrix.append(a)
     return matrix
 
+def createPossibilityCell(s,i,j):
+    p=[]
+    for k in range(1,10):
+        if not findNum(s,i,j,k):
+            p.append(k)
+    return p
+
+def findNextEmptyCell(s,i,j):
+    while s[i][j]!=0:
+        i+=1
+        if i==9:
+            j+=1
+            i=0            
+            if j==9:
+                return (9,9)
+    return (i,j)
+
+def findCell(s,i,j):
+    global rollback
+    (i,j)=findNextEmptyCell(s,i,j)
+    if j==9:
+        print(s)
+        return True
+    p=createPossibilityCell(s,i,j)
+    if len(p)==0:
+        return False
+    print(i,j)
+    for v in p:
+        s[i][j]=v
+        finish=findCell(s,i,j)
+        if finish:
+            return True
+    s[i][j]=0
+
 s=[[5,9,0,0,8,4,0,0,0],
    [0,4,0,0,0,0,0,6,9],
    [0,0,0,1,9,0,8,4,0],
@@ -36,4 +70,5 @@ s=[[5,9,0,0,8,4,0,0,0],
    [0,2,0,3,0,7,9,0,4],
    [0,3,0,0,1,0,0,0,0],
    [6,0,0,5,4,0,0,3,0]]
-print(createPossibilityMatrix(s))
+rollback=[]
+findCell(s,0,0)
